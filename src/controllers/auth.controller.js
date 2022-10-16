0b8f2a1fc09ee8d4cce60a4c68dd1385239ee3c2
hao.nguyensong <hao.nguyensong@powergatesoftware.com>
2022-10-16 16:50:09 +0700

    update authenication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,19 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService } = require('../services');
+const { modelApiResponse } = require('../utils/common');
 
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user, tokens });
+  res.send(modelApiResponse('success', { user, tokens }, 'Register successfully'));
 });
 
 const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPassword(email, password);
   const tokens = await tokenService.generateAuthTokens(user);
-  res.send({ user, tokens });
+  res.send(modelApiResponse('success', { user, tokens }, 'Login successfully'));
 });
 
 const logout = catchAsync(async (req, res) => {
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -14,7 +14,7 @@ const { tokenTypes } = require('../config/tokens');
 const loginUserWithEmailAndPassword = async (email, password) => {
   const user = await userService.getUserByEmail(email);
   if (!user || !(await user.isPasswordMatch(password))) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Incorrect email or password');
   }
   return user;
 };
