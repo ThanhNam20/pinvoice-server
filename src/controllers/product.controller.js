574ac117a8713497867ddbc3cc6b69acda9a0f51
hao.nguyensong <hao.nguyensong@powergatesoftware.com>
2022-11-02 00:42:23 +0700

    update products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,17 +3,18 @@ const { productService } = require('../services');
 const catchAsync = require('../utils/catchAsync');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
+const { modelApiResponse } = require('../utils/common');
 
 const createProduct = catchAsync(async (req, res) => {
   const product = await productService.createProduct(req.body);
-  res.status(httpStatus.CREATED).send(product);
+  res.status(httpStatus.CREATED).send(modelApiResponse('success', product, 'Create product successfully'));
 });
 
 const getProducts = catchAsync(async (req, res) => {
   const filter = pick(req.query, ['name', 'role']);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
   const result = await productService.queryProducts(filter, options);
-  res.status(httpStatus.ACCEPTED).send(result);
+  res.status(httpStatus.CREATED).send(modelApiResponse('success', result, 'Successfully'));
 });
 
 const getProduct = catchAsync(async (req, res) => {
@@ -21,12 +22,12 @@ const getProduct = catchAsync(async (req, res) => {
   if (!product) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
   }
-  res.status(httpStatus.ACCEPTED).send(product);
+  res.status(httpStatus.CREATED).send(modelApiResponse('success', product, 'Successfully'));
 });
 
 const updateProduct = catchAsync(async (req, res) => {
   const product = await productService.updateProductById(req.params.productId, req.body);
-  res.status(httpStatus.ACCEPTED).send(product);
+  res.status(httpStatus.CREATED).send(modelApiResponse('success', product, 'Update product successfully'));
 });
 
 const deleteProduct = catchAsync(async (req, res) => {
