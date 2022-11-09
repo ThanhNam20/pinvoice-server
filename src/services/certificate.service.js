36e1ee3397b07b809c483cc8389f6ddc449aed3f
hao.nguyensong <hao.nguyensong@powergatesoftware.com>
2022-11-09 10:15:58 +0700

    update ca

--- src/services/certificate.service.js
+++ src/services/certificate.service.js
@@ -1,11 +1,14 @@
 const { exec } = require('child_process');
 const { certificatePasswordGenerator } = require('../utils/common');
+
 const genClientCertificate = (userBody) => {
   const { name, commonName, organizationalUnitName, organizationName, localityName, stateOrProvinceName, countryName } =
     userBody;
+  const trimCommonName = commonName.trim();
   const p12Password = certificatePasswordGenerator();
+  console.log(p12Password);
   exec(
-    `cd && cd PKIServer/ && keytool -genkeypair -alias ${name} -keyalg RSA -keysize 2048 -dname "CN=${name}, OU=${organizationalUnitName}, O=${organizationName}, L=${localityName}, ST=${stateOrProvinceName}, C=${countryName}" -keypass ${p12Password} -validity 365 -storetype PKCS12 -keystore ${name}.p12 -storepass ${p12Password} && keytool -certreq -alias ${name} -keypass ${p12Password} -storetype PKCS12 -keystore ${name}.p12 -storepass ${p12Password} -file ${name}.csr && ejbca/bin/ejbca.sh ra addendentity --username ${name} --dn "CN=${name}, OU=${organizationalUnitName}, O=${organizationName}, L=${localityName}, ST=${stateOrProvinceName}, C=${countryName}" --token PEM --caname SERVERCA --type 1 --password ${p12Password} && ejbca/bin/ejbca.sh createcert --username ${name} --password ${p12Password} -c ${name}.csr -f ${name}.pem && keytool -importcert -alias signKey -file ${name}.pem -keystore ${name}.p12 -storepass ${p12Password} -trustcacerts -noprompt && cp -b ${name}.p12 ~/Pinvoice/pinvoice-server/certificates`,
+    `cd && cd PKIServer/ && keytool -genkeypair -alias ${trimCommonName} -keyalg RSA -keysize 2048 -dname "CN=${trimCommonName}, OU=${organizationalUnitName}, O=${organizationName}, L=${localityName}, ST=${stateOrProvinceName}, C=${countryName}" -keypass ${p12Password} -validity 365 -storetype PKCS12 -keystore ${trimCommonName}.p12 -storepass ${p12Password} && keytool -certreq -alias ${trimCommonName} -keypass ${p12Password} -storetype PKCS12 -keystore ${trimCommonName}.p12 -storepass ${p12Password} -file ${trimCommonName}.csr && ejbca/bin/ejbca.sh ra addendentity --username ${trimCommonName} --dn "CN=${trimCommonName}, OU=${organizationalUnitName}, O=${organizationName}, L=${localityName}, ST=${stateOrProvinceName}, C=${countryName}" --token PEM --caname ManagementCA --type 1 --password ${p12Password} && ejbca/bin/ejbca.sh createcert --username ${trimCommonName} --password ${p12Password} -c ${trimCommonName}.csr -f ${trimCommonName}.pem && keytool -importcert -alias signKey -file ${trimCommonName}.pem -keystore ${trimCommonName}.p12 -storepass ${p12Password} -trustcacerts -noprompt && cp -b ${trimCommonName}.p12 ~/Pinvoice/pinvoice-server/certificates`,
     (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error.message}`);
